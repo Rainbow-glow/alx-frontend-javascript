c377a4040e963b2515ee9e6d88a1a760fcf66ffb
Rainbow-glow <emmanuellanweze97@gmail.com>
2024-06-26 22:15:12 +0200

    catch me if you can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  const response = { status: 200, body: 'success' };
   return promise
-    .then(() => (response)
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
-}
